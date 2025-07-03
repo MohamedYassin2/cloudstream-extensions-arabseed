a0f8dfb66af3ffb445868a73a025dd4190b7e37d
MohamedYassin2 <mohamedyassin.s2002@gmail.com>
2025-07-03 17:06:42 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.library") version "7.3.1" apply false
+    kotlin("android") version "1.8.10" apply false
+}
