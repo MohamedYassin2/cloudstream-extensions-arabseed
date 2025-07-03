4a57e9ce3594b4ecaa4dee13115e2df93db37619
MohamedYassin2 <mohamedyassin.s2002@gmail.com>
2025-07-03 17:07:06 +0200

    Create build.gradle.kts

---
+++ Arabseed/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+cloudstream {
+    language = "ar"
+    description = "إضافة Arabseed لمشاهدة الأفلام"
+    authors = listOf("اسمك")
+    status = 1
+    tvTypes = listOf(TvType.Movie)
+    iconUrl = "https://a.asd.homes/favicon.ico"
+}
